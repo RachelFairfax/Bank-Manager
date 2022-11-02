db9071e628544f63bee6ad3f91a84d6e7a0ea2af
RachelFairfax <fairfaxr1@gmail.com>
2022-11-02 13:05:51 +0000

    final commit

--- Application.py
+++ Application.py
@@ -1,57 +1,54 @@
-import Utils as A
+import Utils
 
-def main():
-    Quit = False
-    print("WELCOME TO OUR BANKING APP \n \n")
-    while Quit == False:
-        Quit = menu()
-    print("\n")
-    print("We are sorry to see you go. Have a nice day :)")
+INVALID_CHOICE_MESSAGE = "Invalid response. Try again."
+RUN_AGAIN_MESSAGE = "Would you like to try another option? Type YES or NO: "
+MENU_MESSAGE =  "\n How can we help you today? \n " \
+                "1. Add user \n " \
+                "2. Delete user \n " \
+                "3. Check balance \n " \
+                "4. Show address \n " \
+                "5. Edit details \n " \
+                "6. Update balance \n " \
+                "7. Output all users with the same name \n " \
+                "8. Get all users with more money than a given amount \n " \
+                "9. Display all users \n" \
+                "10. Quit \n"
+WELCOME_MESSAGE = "WELCOME TO OUR BANKING APP "
+INPUT_CHOICE_MESSAGE = "Please enter your choice (between 1 and 9): "
+GOODBYE_MESSAGE = "We are sorry to see you go. \n Have a nice day :)"
+
     
 def menu():
-    INVALID_CHOICE_MESSAGE = "Invalid response. Try again."
-    RUN_AGAIN_MESSAGE = "Would you like to try another option? Type YES or NO: "
-    print("How can we help you today? \n")
-    print("1. Add user")
-    print("2. Delete user")
-    print("3. Check balance")
-    print("4. Show address")
-    print("5. Edit details")
-    print("6. Update balance")
-    print("7. Output all users with the same name")
-    print("8. Get all users with more money than a given amount")
-    print("9. Display all users \n")
 
-    choice = int(input("Please enter your choice (between 1 and 9): "))
-    while choice < 1 or choice > 9:
-        print(INVALID_CHOICE_MESSAGE)
-        choice = input("Please enter your choice: ")
-    if choice == 1:
-        A.Add_User()
-    elif choice == 2:
-        A.Delete_User()
-    elif choice == 3:
-        A.Show_Money()
-    elif choice == 4:
-        A.Show_Address()
-    elif choice == 5:
-        A.Edit_Details()
-    elif choice == 6:
-        A.Update_Balance()
-    elif choice == 7:
-        A.Same_Name()
-    elif choice == 8:
-        A.More_Money()
-    elif choice == 9:
-        A.Display_Users()
+    while True:
+        print(MENU_MESSAGE)
+        choice = input(INPUT_CHOICE_MESSAGE)
+        if choice == '1':
+            Utils.Add_User()
+        elif choice == '2':
+            Utils.Delete_User()
+        elif choice == '3':
+            Utils.Show_Money()
+        elif choice == '4':
+            Utils.Show_Address()
+        elif choice == '5':
+            Utils.Edit_Details()
+        elif choice == '6':
+            Utils.Update_Balance()
+        elif choice == '7':
+            Utils.Same_Name()
+        elif choice == '8':
+            Utils.More_Money()
+        elif choice == '9':
+            Utils.Display_Users()
+        elif choice == '10':
+            print(GOODBYE_MESSAGE)
+            break
+        else:
+            print(INVALID_CHOICE_MESSAGE)
 
-    run_again = input(RUN_AGAIN_MESSAGE)
-    while run_again != "YES" and run_again != "NO":
-        print(INVALID_CHOICE_MESSAGE)
-        run_again = input(RUN_AGAIN_MESSAGE)
-    if run_again == "YES":
-        return False
-    else:
-        return True
+def main():
+    print(WELCOME_MESSAGE)
+    menu()
 
 main()
